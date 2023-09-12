ac0608189cc1bba8805b4a5f8de02501571330ea
AbhigyaBikramMalla <117446967+AbhigyaBikramMalla@users.noreply.github.com>
2023-09-12 17:02:51 +0545

    Add files via upload
    The project directory structure is organized as follows:
    
        Model Folder: This folder contains two important files:
            trained_model.pickle: This is a pickle file containing the trained model.
            columns.json: This JSON file stores information about the columns in the data, specifically, the locations.
    
        Server Folder: Inside this folder, you'll find the server.py file. This Python script runs a Flask server, handling requests and responses for your application.
    
        Templates Folder: This folder houses the frontend files, including:
            index.html: This is the main HTML file for your web application.
            style.css: The CSS file responsible for styling the frontend.
            script.js: The JavaScript file containing client-side logic for your web application.
    
    With this structured directory, your project separates the model and data handling in the Model folder, the server logic in the Server folder, and the frontend components in the Templates folder, making it easier to manage and maintain your application.

---
+++ server/server.py
@@ -0,0 +1,41 @@
+import os
+import util
+from flask import Flask, request, jsonify, render_template
+
+app = Flask(__name__)
+
+# Set the template folder relative to the location of server.py
+template_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'templates'))
+app = Flask(__name__, template_folder=template_dir)
+
+@app.route('/get_location_names', methods=['GET'])
+def get_location_names():
+    response = jsonify({
+        'locations': util.get_location_names()
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
+
+@app.route('/predict_home_price', methods=['GET', 'POST'])
+def predict_home_price():
+    total_sqft = float(request.form['total_sqft'])
+    location = request.form['location']
+    bhk = int(request.form['bhk'])
+    bath = int(request.form['bath'])
+
+    response = jsonify({
+        'estimated_price': util.get_estimated_price(location, total_sqft, bhk, bath)
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
+
+@app.route('/')
+def index():
+    return render_template('app.html')
+
+if __name__ == "__main__":
+    print("Starting Python Flask Server For Home Price Prediction...")
+    util.load_saved_artifacts()
+    app.run()
